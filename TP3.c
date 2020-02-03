0f71ccbeecc810ac8f329aff08850643ad11f898
CAULETGreg <59962725+CAULETGreg@users.noreply.github.com>
2020-02-03 22:19:01 +0100

    1

--- TP3.c
+++ TP3.c
@@ -1,38 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+
 struct Ocject{
 
 	int price;
-	char name [30];
+	char name[30];
 	
 };
 
 typedef struct Ocject generate;
 
-void sorting( int i, int j,    ){
-	
-	
-	
-}
-
 int main(){
 	
-	int z;
-	int iSize = 4;
-	
+	int choice;
+	int nbOject = 4;
+	int i,j,z;
 	
 	generate banana = { 5, "banana" };
 	generate sword = { 100, "sword" };
 	generate rawMeat = { 5, "raw meat" };
 	generate potion = { 30, "potion" };
 	
-	generate inventory[iSize] = { banana, sword, rawMeat, potion };
+	generate c = { 0, "   " };
 	
-	for ( z = 0 ; z == iSize ; z++ ){
+	generate inventory[9];
+	inventory[0] = banana;
+	inventory[1] = sword;
+	inventory[2] = rawMeat;
+	inventory[3] = potion;
+	
+	while ( choice != 3 ){
 		
-		printf("%s\n", inventory->name)
-		printf("%d gold\n", inventory->price)
+		printf("Open inventory - 1\nBuy something from Seller - 2\nExit - 3\n");
+		scanf("%d", &choice);
+		
+		if ( choice == 1 ){
+			
+			for(i=0;i<nbOject-1;i++){
+				
+				for(j=i+1;j<nbOject;j++){
+					
+					if ( strcmp ( inventory[i].name, inventory[j].name ) > 0 ){
+						
+						strcpy ( c.name, inventory[i].name );
+						c.price = inventory[i].price;
+						
+						strcpy ( inventory[i].name, inventory[j].name );
+						inventory[i].price = inventory[j].price;
+						
+						strcpy ( inventory[j].name, c.name );
+						inventory[j].price = c.price;
+					}
+				
+				}
+			
+			}
+			
+			for ( z = 0 ; z < nbOject ; z++ ){
+		
+				printf("%s\n", inventory[z].name);
+				printf("%d gold\n", inventory[z].price);
 	
+			}
+		
+		}
+
+		
+			
+			
+			
+			
 	}
-	
 	return 0;
 	
 }
